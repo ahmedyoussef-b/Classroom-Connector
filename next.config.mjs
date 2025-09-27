8567b90659e4b58681d8f4a350d707a31d607377
bee bees <ahmedyoussefabbes@gmail.com>
2025-09-27 17:25:25 +0000

    ▲ Next.js 14.2.33   - Local:        http://localhost:3000   - Environmen

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   experimental: {
     allowedDevOrigins: [
-        "https://6000-firebase-studio-1756840928840.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev",
-    ]
-  }
+      '3000-firebase-studio-1756840928840.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
