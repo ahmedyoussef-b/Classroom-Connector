57d78ff4a006f6d28d74c769ed25f916710caf2c
bee bees <ahmedyoussefabbes@gmail.com>
2025-09-27 18:10:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,16 @@
 import type {Config} from 'tailwindcss';
+const {fontFamily} = require('tailwindcss/defaultTheme');
 
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
