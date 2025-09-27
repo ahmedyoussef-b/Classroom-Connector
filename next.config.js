a451c821db5243a2a90a98df360220976899649e
bee bees <ahmedyoussefabbes@gmail.com>
2025-09-27 18:35:47 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Votre configuration existante peut être ici
+  experimental: {
+    serverActions: {
+      allowedOrigins: [],
+    },
+    esmExternals: 'loose',
+  },
 };
 
 module.exports = nextConfig;
