bf38193204572a8cf1389296c22846d5e7759600
bee bees <ahmedyoussefabbes@gmail.com>
2025-09-28 13:40:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type {Config} from 'tailwindcss';
-const {fontFamily} = require('tailwindcss/defaultTheme');
+import {fontFamily} from 'tailwindcss/defaultTheme';
 
-export default {
+const config: Config = {
   darkMode: ['class'],
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -97,4 +97,6 @@ export default {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
+
+export default config;
